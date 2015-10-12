8e0b0ff0f78fa7849f075f0585a5b7a8471a926e
qianfeng <qianfeng@Sandy.local>
2015-10-12 11:09:44 +0800

    init commit

---
+++ MyCode/MyCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyCode
+//
+//  Created by qianfeng on 15/10/12.
+//  Copyright (c) 2015年 苗军艳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
